9886798551756485179bdec39a37526c149ef896
Jogvan <jogvan@gmail.com>
2022-01-31 20:38:03 +0000

    Add the function to dynamically create the canvas with size from user input

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # etch-a-sketch
 A Etch-A-Sketch website.
 
-The website will have a canvas with a variable number of "pixels" in a grid up to a maximum of 100x100.
-When the mouse moves over the blocks they will change color. Initially to black but later add option to have random colors
+Create a canvas with variable pixel size. User inputs a value to decide how many pixels should be inside the canvas.
+When the mouse moves over the blocks they will change color. Initially to black but later add option to have random colors.
 
 This project is based on the requirements of [The Odin Project's JavaScript Foundations course](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/etch-a-sketch-project).
--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-const pixels = document.querySelectorAll(".pxl");
+let pixels = document.querySelectorAll(".pxl");
+let size;
 
-pixels.forEach(pixel => pixel.addEventListener("mouseenter", paintItBlack))
+const canvas = document.querySelector(".canvas");
 
-function paintItBlack(event){
+pixels.forEach(pixel => pixel.addEventListener("mouseenter", paintItBlack));
+
+function paintItBlack(event) {
     console.log(event.target);
     event.target.style.backgroundColor = "black";
 }
+
+function createGrid(size) {
+    
+    let oldCanvas = document.querySelector(".canvas");
+    while(oldCanvas.firstChild) {
+        oldCanvas.removeChild(oldCanvas.firstChild);
+    }
+
+    for(let i = 0; i < size; i++){
+        let div = document.createElement("div");
+        div.classList.add("row");
+
+        for(let j = 0; j < size; j++){
+            let pxldiv = document.createElement("div");
+            pxldiv.classList.add('pxl');
+            pxldiv.style.height = 500/size + "px";
+            pxldiv.style.width = 500/size + "px";
+            div.appendChild(pxldiv);
+        }
+        canvas.appendChild(div);
+    }
+    pixels = document.querySelectorAll(".pxl");
+    pixels.forEach(pixel => pixel.addEventListener("mouseenter", paintItBlack));
+}
+
+function getCanvasSize(){
+    let size = window.prompt("How big should the grid be? Please use numbers from between 2-100");
+    return size;
+}
+
+createGrid(getCanvasSize());
