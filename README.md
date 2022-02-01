89ae51d1ae61adacdcbcdefaf798eca474385b81
Jogvan <jogvan@gmail.com>
2022-02-01 00:48:00 +0000

    Add a reset button that clears grid and promts user for a new grid size Change so blocks turn into random colour when passing. After each pass the colour gets darker until it is completely black. Update README.md

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # etch-a-sketch
 A Etch-A-Sketch website.
 
-Create a canvas with variable pixel size. User inputs a value to decide how many pixels should be inside the canvas.
-When the mouse moves over the blocks they will change color. Initially to black but later add option to have random colors.
+Starts with a 16x16 canvas. When the user resets the canvas they can change number from to anywhere between 2 and 100.
+When the mouse moves over the blocks they will change color. Over subsequent passes they will get darker each time by reducing each colour value inside rgba() by 25.
 
 This project is based on the requirements of [The Odin Project's JavaScript Foundations course](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/etch-a-sketch-project).
--- script.js
+++ script.js
@@ -5,38 +5,83 @@ const canvas = document.querySelector(".canvas");
 
 pixels.forEach(pixel => pixel.addEventListener("mouseenter", paintItBlack));
 
+function paintItRGB(event) {
+    // console.log(event.target.style.backgroundColor);
+    //Check if it had any colour prior to event.
+    if (event.target.style.backgroundColor == "rgba(0, 0, 0, 0)") {
+        event.target.style.backgroundColor = `rgba(${randomNumber()},${randomNumber()},${randomNumber()},1)`;
+    }
+    else {
+        event.target.style.backgroundColor = youWantItDarker(event.target.style.backgroundColor);
+    }
+}
+
+function randomNumber() {
+    return Math.floor(Math.random() * 256)
+}
+
+//Takes a rgba string and makes it darker
+function youWantItDarker(rgbaString) { //We kill the flame
+    let rgb = [0, 0, 0];
+
+    let values = rgbaString.split("(");
+    values = values[1].split(",");
+    for (let i = 0; i < 3; i++) {
+        if (parseInt(values[i]) < 25) {
+            rgb[i] = 0;
+            // console.log("yep");
+        }
+        else {
+            rgb[i] = parseInt(values[i]) - 25
+            // console.log("nop");
+        }
+    }
+    console.log(`rgba(${rgb[0]},${rgb[1]},${rgb[2]},1)`);
+    return `rgba(${rgb[0]},${rgb[1]},${rgb[2]},1)`;
+}
+
 function paintItBlack(event) {
-    console.log(event.target);
     event.target.style.backgroundColor = "black";
 }
 
 function createGrid(size) {
     
+    if(isNaN(size)) return;
+
     let oldCanvas = document.querySelector(".canvas");
-    while(oldCanvas.firstChild) {
+    while (oldCanvas.firstChild) {
         oldCanvas.removeChild(oldCanvas.firstChild);
     }
 
-    for(let i = 0; i < size; i++){
+    for (let i = 0; i < size; i++) {
         let div = document.createElement("div");
         div.classList.add("row");
 
-        for(let j = 0; j < size; j++){
+        for (let j = 0; j < size; j++) {
             let pxldiv = document.createElement("div");
             pxldiv.classList.add('pxl');
-            pxldiv.style.height = 500/size + "px";
-            pxldiv.style.width = 500/size + "px";
+            pxldiv.style.height = 500 / size + "px";
+            pxldiv.style.width = 500 / size + "px";
+            pxldiv.style.backgroundColor = "rgba(0,0,0,0)";
             div.appendChild(pxldiv);
         }
         canvas.appendChild(div);
     }
     pixels = document.querySelectorAll(".pxl");
-    pixels.forEach(pixel => pixel.addEventListener("mouseenter", paintItBlack));
+    //TODO Make button to change eventlistener
+    // pixels.forEach(pixel => pixel.addEventListener("mouseenter", paintItBlack));
+    pixels.forEach(pixel => pixel.addEventListener("mouseenter", paintItRGB));
 }
 
-function getCanvasSize(){
+function getCanvasSize() {
     let size = window.prompt("How big should the grid be? Please use numbers from between 2-100");
+
+    if(isNaN(size) || size < 2 || size > 100){
+        alert ("Not a valid number!");
+        return;
+    }
     return size;
 }
 
-createGrid(getCanvasSize());
+createGrid(16);
+
